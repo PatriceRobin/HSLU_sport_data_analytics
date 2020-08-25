2c3040572ee68ea620a2e4aff96fd69dfd74a153
PatriceRobin <62515061+PatriceRobin@users.noreply.github.com>
2020-08-25 10:35:08 +0200

    ✨jupyter file

--- nba_season2019_2020.py
+++ nba_season2019_2020.py
@@ -1,30 +1,50 @@
 # load packages
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from tabulate import tabulate
 import pandas as pd
+import requests
 
 # NBA season we will be analyzing
 year = 2019  # URL page we will scraping (see image above)
 # this is the HTML from the given URL
-url = "https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_2020.html%3Fsr%26utm_source%3Ddirect%26utm_medium%3DShare%26utm_campaign%3DShareTool&div=div_team-stats-per_game"
-html = urlopen(url)
+url = "https://www.basketball-reference.com/boxscores/"
 
-soup = BeautifulSoup(html, 'html.parser')
+page = requests.get(url)
+data = page.text
+
+
+soup = BeautifulSoup(page.content, 'html.parser')
+soup.prettify
+
+soup
+
 
 # use findALL() to get the column headers
-# use getText()to extract the text we need into a list
-soup.findAll('tr', limit=2)
-# exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
-headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-headers = headers[1:]
+
+row = soup.find_all('tr', attrs = {'class': 'poptip right'})
+print(row)
+
+
+for data in row.find_all('td'):
+    print(data.get_text())
+
+
+
+headers = headers[0:4]
 print(headers)
 
+soup.findAll('tr', limit=3)# use getText()to extract the text we need into a list
+
+
+header = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]# exclude the first column as we will not need the ranking order from Basketball Reference for the analysis
+print(headers)
 
 # avoid the first header row
 rows = soup.findAll('tr')[1:]
-nba_season_2021 = [[td.getText() for td in rows[i].findAll('td')]
+game_results = [[td.getText() for td in rows[i].findAll('td')]
                 for i in range(len(rows))]
 
+print(game_results)
 
-stats = pd.DataFrame(player_stats, columns=headers)
-print(stats.head(10))
+game_stats = pd.DataFrame(game_results, columns=headers)
